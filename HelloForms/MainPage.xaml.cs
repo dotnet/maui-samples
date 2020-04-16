2342fceebc3e9459e4356eb1c5d303c67e6dc0b9
Jonathan Peppers <jonathan.peppers@microsoft.com>
2020-04-16 13:58:20 -0500

    Add Xamarin.Forms samples

---
+++ HelloForms/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HelloForms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
