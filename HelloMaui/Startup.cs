97615d30c45e7ae07fdccb75864e87fc49b57baf
Jonathan Dick <jondick@gmail.com>
2021-04-06 10:41:32 -0400

    Add lifecycle event example

--- HelloMaui/Startup.cs
+++ HelloMaui/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace HelloMaui
 {
@@ -13,6 +14,15 @@ namespace HelloMaui
 				.UseMauiApp<App>()
 				.ConfigureFonts(fonts => {
 					fonts.AddFont("ionicons.ttf", "IonIcons");
+				})
+				.ConfigureLifecycleEvents(lifecycle => {
+					#if ANDROID
+					lifecycle.AddAndroid(d => {
+						d.OnBackPressed(activity => {
+							System.Diagnostics.Debug.WriteLine("Back button pressed!");
+						});
+					});
+					#endif
 				});
 		}
 	}
