386c07373bf5d6e5c0ebf5e93d39cd96abc6197d
Shane Neuville <shneuvil@microsoft.com>
2021-04-07 10:26:31 -0500

    - fix tabs

--- HelloMaui/App.xaml.cs
+++ HelloMaui/App.xaml.cs
@@ -5,7 +5,7 @@ using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
 namespace HelloMaui
 {
 	public partial class App : Microsoft.Maui.Controls.Application
-    {
+	{
 		public App()
 		{
 			InitializeComponent();
