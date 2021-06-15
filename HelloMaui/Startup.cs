89545dd1bfb9ce5c3e6b04d8a59fd802000f66ed
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-06-15 09:20:12 -0500

    Bump to .NET MAUI 6.0.100-preview.5.763 (#167)
    There is a minor API break, a new `using` statement is required. `@(MauiXaml)` is now used for .xaml files.
    
    I also made some minor fixes in `.csproj` files.

--- HelloMaui/Startup.cs
+++ HelloMaui/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.LifecycleEvents;
 
 namespace HelloMaui
