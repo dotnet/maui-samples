936e601ccedd6d49f0da31bf61f2d9ddd743e529
David Ortinau <david.ortinau@microsoft.com>
2022-05-06 13:36:06 -0500

    WeatherTwentyOne app

---
+++ 6.0/Apps/WeatherTwentyOne/src/WeatherTwentyOne/Platforms/Windows/TrayService.cs
@@ -0,0 +1,21 @@
+﻿using Hardcodet.Wpf.TaskbarNotification.Interop;
+using Microsoft.UI.Xaml;
+using WeatherTwentyOne.Services;
+
+namespace WeatherTwentyOne.WinUI;
+
+public class TrayService : ITrayService
+{
+    WindowsTrayIcon tray;
+
+    public Action ClickHandler { get; set; }
+
+    public void Initialize()
+    {
+        tray = new WindowsTrayIcon("Platforms/Windows/trayicon.ico");
+        tray.LeftClick = () => {
+            WindowExtensions.BringToFront();
+            ClickHandler?.Invoke();
+        };
+    }
+}
