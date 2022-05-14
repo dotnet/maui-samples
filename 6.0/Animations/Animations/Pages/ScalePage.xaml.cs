9168d6a72e1a40a5a19949c9b223f5882c4f9b5e
David Ortinau <david.ortinau@microsoft.com>
2022-05-13 21:11:51 -0500

    Animations sample (#200)
    * first batch of animations
    
    * reorg, added custom anim extension

---
+++ 6.0/Animations/Animations/Pages/ScalePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Animations;
+
+public partial class ScalePage : ContentPage
+{
+	public ScalePage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnClickedAsync(object sender, EventArgs e)
+	{
+		await BotImg.ScaleTo(0, easing:Easing.SpringIn);
+        await Task.Delay(1000);
+        await BotImg.ScaleTo(1, easing: Easing.SpringOut);
+	}
+}
+
