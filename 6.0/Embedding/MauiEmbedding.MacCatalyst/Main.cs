e91d19f66e119989edbd6eabaa97a4ed0086b085
David Ortinau <david.ortinau@microsoft.com>
2022-05-26 20:29:58 -0500

    adding Mac Catalyst

---
+++ 6.0/Embedding/MauiEmbedding.MacCatalyst/Main.cs
@@ -0,0 +1,7 @@
+﻿using MauiEmbedding.MacCatalyst;
+
+// This is the main entry point of the application.
+// If you want to use a different Application Delegate class from "AppDelegate"
+// you can specify it here.
+UIApplication.Main (args, null, typeof (AppDelegate));
+
