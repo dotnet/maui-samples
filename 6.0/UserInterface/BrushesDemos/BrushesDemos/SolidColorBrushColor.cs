4411622809229e7e13aebee190cbf872af909437
David Britch <davidbritch@users.noreply.github.com>
2022-06-24 14:54:42 +0100

    Brushes sample.

---
+++ 6.0/UserInterface/BrushesDemos/BrushesDemos/SolidColorBrushColor.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+
+namespace BrushesDemos
+{
+    public class SolidColorBrushColor : IEquatable<SolidColorBrushColor>, IComparable<SolidColorBrushColor>
+    {
+        public string Name { get; private set; }
+        public Color Color { get; private set; }
+        public string Hex { get; private set; }
+
+        public static IList<SolidColorBrushColor> All { get; private set; }
+
+        public bool Equals(SolidColorBrushColor other)
+        {
+            return Name.Equals(other.Name);
+        }
+
+        public int CompareTo(SolidColorBrushColor other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+
+        static SolidColorBrushColor()
+        {
+            List<SolidColorBrushColor> all = new List<SolidColorBrushColor>();
+
+            // Loop through the properties of the Brush class
+            foreach (PropertyInfo propertyInfo in typeof(Brush).GetRuntimeProperties())
+            {
+                if (propertyInfo.PropertyType == typeof(SolidColorBrush))
+                {
+                    // Instantiate a SolidColorBrushColor object
+                    SolidColorBrush brush = (SolidColorBrush)propertyInfo.GetValue(null);
+
+                    SolidColorBrushColor brushColor = new SolidColorBrushColor
+                    {
+                        Name = propertyInfo.Name,
+                        Color = brush.Color,
+                        Hex = brush.Color.ToHex()
+                    };
+
+                    all.Add(brushColor);
+                }
+            }
+
+            all.TrimExcess();
+            all.Sort();
+            All = all;
+        }
+    }
+}
---
+++ 6.0/UserInterface/BrushesDemos/README.md
@@ -0,0 +1,19 @@
+---
+name: .NET MAUI - Brushes
+description: This sample demonstrates how to paint an area with a .NET MAUI brush.
+page_type: sample
+languages:
+- csharp
+- xaml
+products:
+- dotnet-maui
+urlFragment: userinterface-brushes
+---
+
+# Consume a REST-based web service
+
+A .NET Multi-platform App UI (.NET MAUI) brush enables you to paint an area, such as the background of a control, using different approaches.
+
+This sample demonstrates how to paint areas with .NET MAUI brushes.
+
+For more information about this sample, see [.NET MAUI Brushes](https://docs.microsoft.com/dotnet/maui/user-interface/brushes/).
