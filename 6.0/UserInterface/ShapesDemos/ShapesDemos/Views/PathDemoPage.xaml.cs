e05219482f39726d532105e8fc392d4b81d6b39c
David Britch <davidbritch@users.noreply.github.com>
2022-06-24 15:00:28 +0100

    Shapes sample.

---
+++ 6.0/UserInterface/ShapesDemos/ShapesDemos/Views/PathDemoPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShapesDemos.Views
+{
+    public partial class PathDemoPage : ContentPage
+    {
+        public PathDemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 6.0/UserInterface/ShapesDemos/ShapesDemos/Views/SpiralRunnerDemoPage.cs
@@ -0,0 +1,27 @@
+﻿namespace ShapesDemos.Views
+{
+    public class SpiralRunnerDemoPage : SpiralDemoPage
+    {
+        Timer timer;
+
+        public SpiralRunnerDemoPage()
+        {
+            polyline.StrokeDashArray.Add(4);
+            polyline.StrokeDashArray.Add(2);
+            double total = polyline.StrokeDashArray[0] + polyline.StrokeDashArray[1];
+
+            timer = new Timer(new TimerCallback((s) =>
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    double secs = DateTime.Now.TimeOfDay.TotalSeconds;
+                    polyline.StrokeDashOffset = total * (secs % 1);
+                })),
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromMilliseconds(15));
+        }
+
+        ~SpiralRunnerDemoPage() => timer.Dispose();
+    }
+}
+
