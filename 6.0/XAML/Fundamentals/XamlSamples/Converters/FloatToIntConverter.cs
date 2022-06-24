15f660939ae19c7e1a519bda85556ad287d27076
David Britch <davidbritch@users.noreply.github.com>
2022-06-24 14:38:15 +0100

    XAML fundamentals sample.

---
+++ 6.0/XAML/Fundamentals/XamlSamples/Converters/FloatToIntConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace XamlSamples
+{
+    public class FloatToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float multiplier;
+
+            if (!float.TryParse(parameter as string, out multiplier))
+                multiplier = 1;
+
+            return (int)Math.Round(multiplier * (float)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float divider;
+
+            if (!float.TryParse(parameter as string, out divider))
+                divider = 1;
+
+            return ((float)(int)value) / divider;
+        }
+    }
+}
---
+++ 6.0/XAML/Fundamentals/XamlSamples/NamedColor.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace XamlSamples
+{
+    public class NamedColor
+    {
+        public string Name { private set; get; }
+
+        public string FriendlyName { private set; get; }
+
+        public Color Color { private set; get; }
+
+        // Expose the Color fields as properties
+        public float Red => Color.Red;
+        public float Green => Color.Green;
+        public float Blue => Color.Blue;
+
+        // Static members
+        static NamedColor()
+        {
+            List<NamedColor> all = new List<NamedColor>();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            // Loop through the public static fields of the Color structure.
+            foreach (FieldInfo fieldInfo in typeof (Colors).GetRuntimeFields ())
+            {
+                if (fieldInfo.IsPublic && 
+                    fieldInfo.IsStatic &&
+                    fieldInfo.FieldType == typeof (Color))
+                {
+                    // Convert the name to a friendly name.
+                    string name = fieldInfo.Name;
+                    stringBuilder.Clear();
+                    int index = 0;
+
+                    foreach (char ch in name)
+                    {
+                        if (index != 0 && Char.IsUpper(ch))
+                        {
+                            stringBuilder.Append(' ');
+                        }
+                        stringBuilder.Append(ch);
+                        index++;
+                    }
+
+                    // Instantiate a NamedColor object.
+                    NamedColor namedColor = new NamedColor
+                    {
+                        Name = name,
+                        FriendlyName = stringBuilder.ToString(),
+                        Color = (Color)fieldInfo.GetValue(null)
+                    };
+
+                    // Add it to the collection.
+                    all.Add(namedColor);
+                }
+            }
+            all.TrimExcess();
+            All = all;
+        }
+
+        public static IEnumerable<NamedColor> All { private set; get; }
+    }
+}
