60badd1181b4108dbe419b6a3cb31972be2cef03
David Britch <davidbritch@users.noreply.github.com>
2022-06-29 16:25:17 +0100

    Layout samples.

---
+++ 6.0/UserInterface/Layouts/AbsoluteLayoutDemos/AbsoluteLayoutDemos/Views/Code/ProportionalDemoPage.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Maui.Layouts;
+
+namespace AbsoluteLayoutDemos.Views.Code
+{
+    public class ProportionalDemoPage : ContentPage
+    {
+        public ProportionalDemoPage()
+        {
+            BoxView blue = new BoxView { Color = Colors.Blue };
+            AbsoluteLayout.SetLayoutBounds(blue, new Rect(0.5, 0, 100, 25));
+            AbsoluteLayout.SetLayoutFlags(blue, AbsoluteLayoutFlags.PositionProportional);
+
+            BoxView green = new BoxView { Color = Colors.Green };
+            AbsoluteLayout.SetLayoutBounds(green, new Rect(0, 0.5, 25, 100));
+            AbsoluteLayout.SetLayoutFlags(green, AbsoluteLayoutFlags.PositionProportional);
+
+            BoxView red = new BoxView { Color = Colors.Red };
+            AbsoluteLayout.SetLayoutBounds(red, new Rect(1, 0.5, 25, 100));
+            AbsoluteLayout.SetLayoutFlags(red, AbsoluteLayoutFlags.PositionProportional);
+
+            BoxView black = new BoxView { Color = Colors.Black };
+            AbsoluteLayout.SetLayoutBounds(black, new Rect(0.5, 1, 100, 25));
+            AbsoluteLayout.SetLayoutFlags(black, AbsoluteLayoutFlags.PositionProportional);
+
+            Label label = new Label { Text = "Centered text" };
+            AbsoluteLayout.SetLayoutBounds(label, new Rect(0.5, 0.5, 110, 25));
+            AbsoluteLayout.SetLayoutFlags(label, AbsoluteLayoutFlags.PositionProportional);
+
+            Title = "Proportional demo";
+            Content = new AbsoluteLayout
+            {
+                Children =
+                {
+                    blue,
+                    green,
+                    red,
+                    black,
+                    label
+                }
+            };
+        }
+    }
+}
+
---
+++ 6.0/UserInterface/Layouts/BindableLayoutDemos/BindableLayoutDemos/Templates/AchievementsDataTemplateSelector.cs
@@ -0,0 +1,14 @@
+﻿namespace BindableLayoutDemos.Templates
+{
+    public class AchievementsDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultTemplate { get; set; }
+        public DataTemplate OtherTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            bool result = (bool)item;
+            return result ? OtherTemplate : DefaultTemplate;            
+        }
+    }
+}
