7332fda6cbca7c47e55f58047946b2607086b445
David Britch <davidbritch@users.noreply.github.com>
2022-07-01 14:28:00 +0100

    GraphicsView sample.

---
+++ 6.0/UserInterface/Views/GraphicsViewDemos/GraphicsViewDemos/Views/BlendModesPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphicsViewDemos.Views
+{
+    public partial class BlendModesPage : ContentPage
+    {
+        public BlendModesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
