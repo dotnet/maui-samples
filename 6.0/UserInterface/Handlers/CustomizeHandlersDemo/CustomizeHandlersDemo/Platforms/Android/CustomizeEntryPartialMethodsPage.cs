4442cd139ddefcacbfc617c8448d9be7fdc84d4e
David Britch <davidbritch@users.noreply.github.com>
2022-08-12 11:54:48 +0100

    Customise handlers sample (#246)
    * Customize a handler sample.
    
    * Fornatted files.

---
+++ 6.0/UserInterface/Handlers/CustomizeHandlersDemo/CustomizeHandlersDemo/Platforms/Android/CustomizeEntryPartialMethodsPage.cs
@@ -0,0 +1,12 @@
+﻿using AndroidX.AppCompat.Widget;
+
+namespace CustomizeHandlersDemo.Views
+{
+    public partial class CustomizeEntryPartialMethodsPage : ContentPage
+    {
+        partial void ChangedHandler(object sender, EventArgs e)
+        {
+            ((sender as Entry).Handler.PlatformView as AppCompatEditText).SetSelectAllOnFocus(true);
+        }
+    }
+}
