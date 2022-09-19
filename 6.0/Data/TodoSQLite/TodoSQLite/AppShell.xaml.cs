219f869879d039c9a35d7cff0bf336055e282bd9
James Montemagno <james.montemagno@gmail.com>
2022-09-19 13:38:31 -0700

    Add sqlite sample

---
+++ 6.0/Data/TodoSQLite/TodoSQLite/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using TodoSQLite.Views;
+
+namespace TodoSQLite;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute(nameof(TodoItemPage), typeof(TodoItemPage));
+	}
+}
