b5ebd6a741b28dfe414c940aa51d1cdeadb8fc12
James Montemagno <james.montemagno@gmail.com>
2022-09-20 05:34:58 -0700

    Add sqlite sample (#272)

---
+++ 6.0/Data/TodoSQLite/README.md
@@ -0,0 +1,17 @@
+---
+name: .NET MAUI - Local Database with SQLite
+description: This sample demonstrates how to store data in a local database using SQLite.
+page_type: sample
+languages:
+- csharp
+- xaml
+products:
+- dotnet-maui
+urlFragment: database-sqlite
+---
+
+# Local Database with SQLite
+
+This sample demonstrates how to store data in a local database using SQLite in .NET MAUI applications.
+
+For more information about the sample see [Local Databases with SQlite documentaiton](https://docs.microsoft.com/dotnet/maui/data-cloud/database-sqlite).
---
+++ 6.0/Data/TodoSQLite/TodoSQLite/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace TodoSQLite;
+
+public static class Constants
+{
+    public const string DatabaseFilename = "TodoSQLite.db3";
+
+    public const SQLite.SQLiteOpenFlags Flags =
+        // open the database in read/write mode
+        SQLite.SQLiteOpenFlags.ReadWrite |
+        // create the database if it doesn't exist
+        SQLite.SQLiteOpenFlags.Create |
+        // enable multi-threaded database access
+        SQLite.SQLiteOpenFlags.SharedCache;
+
+    public static string DatabasePath => 
+        Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+}
