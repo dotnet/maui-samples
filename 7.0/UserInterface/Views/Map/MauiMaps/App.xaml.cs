5a0d712e31c72c2137e84efcd9077a0a5268d8c8
David Ortinau <david.ortinau@microsoft.com>
2022-09-20 11:35:05 -0500

    Preview samples for .NET 7 RC1 (#273)
    * context menu
    
    * updated to net7
    
    * 7.0 preview samples for map and context menu
    
    * Tooltip sample
    
    * undoing net7 changes in 6.0 folder

---
+++ 7.0/UserInterface/Views/Map/MauiMaps/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiMaps;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ 7.0/UserInterface/Views/Map/MauiMaps/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiMaps;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
