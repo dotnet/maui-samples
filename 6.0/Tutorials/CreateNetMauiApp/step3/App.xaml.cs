1ec489b0ef4d936315d0f96bd7dee4e57d86bd36
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2022-09-30 09:29:46 -0700

    Add code for each tutorial step (#280)
    * Add code for each tutorial step
    
    * readme
    
    * minor

---
+++ 6.0/Tutorials/CreateNetMauiApp/step3/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Notes;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ 6.0/Tutorials/CreateNetMauiApp/step4/Models/About.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Models;
+
+internal class About
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://aka.ms/maui";
+    public string Message => "This app is written in XAML and C# with .NET MAUI.";
+}
