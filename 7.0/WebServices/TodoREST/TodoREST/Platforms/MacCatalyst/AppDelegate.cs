9339b18db0f4cc381381c717996eef5ecfbee1c0
David Britch <davidbritch@users.noreply.github.com>
2022-11-04 14:11:09 +0000

    Move TodoREST to .NET 7.

---
+++ 7.0/WebServices/TodoREST/TodoREST/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TodoREST;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
