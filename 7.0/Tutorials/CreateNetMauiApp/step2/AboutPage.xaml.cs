d995066b68dc3bf5038f6c192fefb2a75349a2d2
Andy De George (adegeo) <adegeo@microsoft.com>
2022-12-20 14:21:03 -0800

    upgrade 1st tutorial from 6 to 7

---
+++ 7.0/Tutorials/CreateNetMauiApp/step2/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace Notes;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
