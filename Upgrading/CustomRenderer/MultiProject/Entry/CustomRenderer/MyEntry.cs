a9bac25b016828c959a7110cad7426d082827284
David Ortinau <david.ortinau@microsoft.com>
2023-04-07 15:26:48 -0500

    custom renderer example with multiple projects

---
+++ Upgrading/CustomRenderer/MultiProject/Entry/CustomRenderer/MyEntry.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace CustomRenderer
+{
+	public class MyEntry : Entry
+	{
+	}
+}
+	
---
+++ Upgrading/CustomRenderer/MultiProject/Entry/iOS/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Controls.Compatibility.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+
+namespace CustomRenderer.iOS;
+
+public static class MauiProgram
+{
+	public static MauiAppBuilder builder;
+        public static MauiAppBuilder Builder
+        {
+            get
+            {
+                if (builder == null)
+                {
+                    builder = MauiApp.CreateBuilder();
+                }
+
+                return builder;
+            }
+        }
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = Builder;
+		builder
+			.UseMauiCompatibility()
+			.UseMauiApp<App>();
+
+		builder.ConfigureMauiHandlers(handlers => {
+			handlers.AddCompatibilityRenderer(typeof(CustomRenderer.MyEntry), typeof(CustomRenderer.iOS.MyEntryRenderer));	
+		});
+
+		return builder.Build();
+	}
+}
