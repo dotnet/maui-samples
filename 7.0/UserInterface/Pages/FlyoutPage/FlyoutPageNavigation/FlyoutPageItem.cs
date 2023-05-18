5df1b5998fef869071c04c7002f1c530a6c0b8f3
David Britch <davidbritch@users.noreply.github.com>
2023-05-18 15:17:52 +0100

    Add FlyoutPage sample.

---
+++ 7.0/UserInterface/Pages/FlyoutPage/FlyoutPageNavigation/FlyoutPageItem.cs
@@ -0,0 +1,9 @@
+﻿namespace FlyoutPageNavigation
+{
+	public class FlyoutPageItem
+	{
+		public string Title { get; set; }
+		public string IconSource { get; set; }
+		public Type TargetType { get; set; }
+	}
+}
