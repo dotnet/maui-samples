7784545a81bec3bcf39efdc4958ee63b9c7a49ee
David Britch <davidbritch@users.noreply.github.com>
2023-05-19 10:02:13 +0100

    Remove newly added sample and fix up old sample.

--- 7.0/UserInterface/Pages/FlyoutPage/FlyoutPageNavigation/FlyoutPageItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FlyoutPageNavigation
-{
-	public class FlyoutPageItem
-	{
-		public string Title { get; set; }
-		public string IconSource { get; set; }
-		public Type TargetType { get; set; }
-	}
-}
