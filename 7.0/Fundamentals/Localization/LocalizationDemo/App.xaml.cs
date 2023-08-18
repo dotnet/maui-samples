22edbc8b586be79f40a4fdab4ad8985bff1d3333
David Britch <davidbritch@users.noreply.github.com>
2023-08-18 12:53:02 +0100

    Localization sample. (#369)

---
+++ 7.0/Fundamentals/Localization/LocalizationDemo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace LocalizationDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
