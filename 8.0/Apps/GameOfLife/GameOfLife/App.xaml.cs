24c6a40d29dcfcaa670d8d69b1870d3b28e5011d
David Britch <davidbritch@users.noreply.github.com>
2024-02-12 17:05:21 +0000

    Game of Life (#432)
    * New sample.
    
    * Win support.

---
+++ 8.0/Apps/GameOfLife/GameOfLife/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GameOfLife;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ 8.0/Apps/GameOfLife/GameOfLife/GameCell.cs
@@ -0,0 +1,39 @@
+﻿namespace GameOfLife
+{
+    class GameCell : BoxView
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        bool isAlive;
+        public bool IsAlive
+        {
+            get
+            {
+                return isAlive;
+            }
+            set
+            {
+                if (isAlive != value)
+                {
+                    isAlive = value;
+                    BackgroundColor = isAlive ? Colors.Black : Colors.White;
+                }
+            }
+        }
+
+        public event EventHandler Tapped;
+
+        public GameCell()
+        {
+            BackgroundColor = Colors.White;
+
+            TapGestureRecognizer tapGesture = new TapGestureRecognizer();
+            tapGesture.Tapped += (sender, args) =>
+            {
+                Tapped?.Invoke(this, EventArgs.Empty);
+            };
+            GestureRecognizers.Add(tapGesture);
+        }
+    }
+}
