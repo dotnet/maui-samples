3bc23ba89db8be155452ce179500f3e950612f9f
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2024-02-21 11:21:42 +0100

    Enhanced BeginnersSeries sample (MauiApp2) to be more beginner's friendly (#439)
    - Updated solution, project and namespace to 'BeginnersTask' to be aligned with other samples naming structure
    - Fixed missing indents
    - Added comment to MauiProgram.cs with helpful links to Microsoft Learn
    - Added comment to MainViewModel.cs with helpful code pointers to see related setups and receivers of routings

---
+++ 8.0/Beginners-Series/BeginnersTask/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using BeginnersTasks.ViewModel;
+
+namespace BeginnersTasks;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+		
+		// See https://learn.microsoft.com/en-us/dotnet/core/extensions/dependency-injection#service-lifetimes
+		// to understand the differences between [AddSingleton] and [AddTransient].
+		
+		builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+
+		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddSingleton<MainViewModel>();
+
+        builder.Services.AddTransient<DetailPage>();
+        builder.Services.AddTransient<DetailViewModel>();
+
+        return builder.Build();
+	}
+}
---
+++ 8.0/Beginners-Series/BeginnersTask/ViewModel/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace BeginnersTasks.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    readonly IConnectivity connectivity;
+    public MainViewModel(IConnectivity connectivity)
+    {
+        Items = [];
+        this.connectivity = connectivity;
+    }
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+    async Task Add()
+    {
+        // Assure text is not empty
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+
+        // Assure there's an internet connection
+        // else show an alert
+        if(connectivity.NetworkAccess != NetworkAccess.Internet)
+        {
+            await Shell.Current.DisplayAlert("Uh Oh!", "No Internet", "OK");
+            return;
+        }
+
+        // Add text to list of todos
+        Items.Add(Text);
+        
+        // Reset Text
+        Text = string.Empty;
+    }
+
+    [RelayCommand]
+    void Delete(string s)
+    {
+        // If the list of todos contains
+        // given string, remove it from list
+        if(Items.Contains(s))
+        {
+            Items.Remove(s);
+        }
+    }
+
+    [RelayCommand]
+    async Task Tap(string s)
+    {
+        // Trigger a navigation to the detail page
+        //  - See [AppShell] for how to add a routing to the app's navigation
+        //  - See [DetailViewModel] for how to resolve the 'Text' query parameter
+        await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+    }
+}
