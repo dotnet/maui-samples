9f63470e5c9739c6e079bd74c3bbff2cdab391e1
David Britch <davidbritch@users.noreply.github.com>
2024-02-27 15:18:11 +0000

    BugSweeper. (#446)

---
+++ 8.0/Apps/BugSweeper/BugSweeper/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BugSweeper
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
