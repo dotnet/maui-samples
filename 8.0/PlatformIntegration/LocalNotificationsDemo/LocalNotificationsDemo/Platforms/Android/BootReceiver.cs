d0eb6b2f3578a77edea2fbf85823142f1a530f76
David Britch <davidbritch@users.noreply.github.com>
2024-06-18 12:01:41 +0100

    Local notifications (#486)
    * Local notifications sample.
    
    * Tidy up.
    
    * Remove Windows/Tizen files.

---
+++ 8.0/PlatformIntegration/LocalNotificationsDemo/LocalNotificationsDemo/Platforms/Android/BootReceiver.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content;
+
+namespace LocalNotificationsDemo.Platforms.Android;
+
+[BroadcastReceiver(Enabled = true, Label = "Reboot complete receiver", Exported = false)]
+[IntentFilter(new[] { Intent.ActionBootCompleted })]
+public class BootReceiver : BroadcastReceiver
+{
+    public override void OnReceive(Context context, Intent intent)
+    {
+        if (intent.Action == "android.intent.action.BOOT_COMPLETED")
+        {
+            // Recreate alarms
+        }
+    }
+}
+
