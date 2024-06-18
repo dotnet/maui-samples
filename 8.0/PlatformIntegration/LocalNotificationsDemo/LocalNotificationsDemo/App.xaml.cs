963dd0d0a30a54070d649a049590a4377d5af4c6
David Britch <davidbritch@users.noreply.github.com>
2024-06-18 10:12:29 +0100

    Local notifications sample.

---
+++ 8.0/PlatformIntegration/LocalNotificationsDemo/LocalNotificationsDemo/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace LocalNotificationsDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        //protected override Window CreateWindow(IActivationState? activationState)
+        //{
+        //    return new Window(new AppShell());
+        //    //return base.CreateWindow(activationState);
+        //}
+    }
+}
---
+++ 8.0/PlatformIntegration/LocalNotificationsDemo/LocalNotificationsDemo/INotificationManagerService.cs
@@ -0,0 +1,9 @@
+﻿namespace LocalNotificationsDemo;
+
+public interface INotificationManagerService
+{
+    event EventHandler NotificationReceived;
+    void SendNotification(string title, string message, DateTime? notifyTime = null);
+    void ReceiveNotification(string title, string message);
+}
+
