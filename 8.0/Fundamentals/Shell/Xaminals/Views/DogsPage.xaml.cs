3c386df343f0c8c3526f2198ad158a2cbc71d9e3
Jakub Florkowski <kubaflo123@gmail.com>
2024-07-28 10:17:31 +0200

    [Collection view] Handle a reselect scenario

--- 8.0/Fundamentals/Shell/Xaminals/Views/DogsPage.xaml.cs
+++ 8.0/Fundamentals/Shell/Xaminals/Views/DogsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xaminals.Models;
+﻿using Xaminals.Extensions;
+using Xaminals.Models;
 
 namespace Xaminals.Views
 {
@@ -11,6 +12,9 @@ namespace Xaminals.Views
 
         async void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (((CollectionView)sender).ClearSelection())
+                return;
+
             string dogName = (e.CurrentSelection.FirstOrDefault() as Animal).Name;
             // The following route works because route names are unique in this application.
             await Shell.Current.GoToAsync($"dogdetails?name={dogName}");
