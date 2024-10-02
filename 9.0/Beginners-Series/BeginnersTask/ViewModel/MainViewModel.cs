d5f6db83aeb9913c36da305e7c4ccad33b77f5ca
David Britch <davidbritch@users.noreply.github.com>
2024-10-02 10:44:16 +0100

    .NET 9 compliance.

--- 9.0/Beginners-Series/BeginnersTask/ViewModel/MainViewModel.cs
+++ 9.0/Beginners-Series/BeginnersTask/ViewModel/MainViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 
 namespace BeginnersTasks.ViewModel;
 
