1c8f542f9a418172da88413e883319f9fa86c519
David Britch <davidbritch@users.noreply.github.com>
2024-10-11 13:53:47 +0100

    TitleBar demo. (#526)

---
+++ 9.0/UserInterface/Views/TitleBarDemo/TitleBarDemo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TitleBarDemo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
