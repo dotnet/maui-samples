4c183f08c4d9ac4ba25da4ba96bf9213425b77b3
David Britch <davidbritch@users.noreply.github.com>
2024-11-26 12:55:53 +0000

    Fix height.

--- 9.0/UserInterface/Views/CarouselViewDemos/CarouselViewDemos/Views/Sizing/DynamicSizeItemsPage.xaml.cs
+++ 9.0/UserInterface/Views/CarouselViewDemos/CarouselViewDemos/Views/Sizing/DynamicSizeItemsPage.xaml.cs
@@ -12,7 +12,7 @@
             Image image = sender as Image;
             image.HeightRequest = image.WidthRequest = image.HeightRequest.Equals(150) ? 200 : 150;
             Border border = ((Border)image.Parent.Parent);
-            border.HeightRequest = border.HeightRequest.Equals(340) ? 390 : 340;
+            border.HeightRequest = border.HeightRequest.Equals(360) ? 410 : 360;
         }
     }
 }
