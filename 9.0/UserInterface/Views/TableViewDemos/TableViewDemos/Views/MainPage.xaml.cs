689ebb420f6238df086b6ed0d0e49e9e31b9e673
Jonathan Peppers <jonathan.peppers@microsoft.com>
2025-02-18 13:02:02 -0600

    My updated code-behind to match the XAML

--- 9.0/UserInterface/Views/TableViewDemos/TableViewDemos/Views/MainPage.xaml.cs
+++ 9.0/UserInterface/Views/TableViewDemos/TableViewDemos/Views/MainPage.xaml.cs
@@ -6,8 +6,20 @@ public partial class MainPage : ContentPage
 {
 	public ICommand NavigateCommand { get; private set; }
 
+	public object[] Pages { get; private set; }
+
 	public MainPage()
 	{
+		Pages =
+		[
+			new { Text = "Menu page", PageType = typeof(MenuIntentPage) },
+			new { Text = "Settings page", PageType = typeof(SettingsIntentPage) },
+			new { Text = "Form page", PageType = typeof(FormIntentPage) },
+			new { Text = "Data page", PageType = typeof(DataIntentPage) },
+			new { Text = "ImageCell page", PageType = typeof(ImageCellPage) },
+			new { Text = "Right to left TableView", PageType = typeof(RightToLeftTablePage) },
+		];
+
 		InitializeComponent();
 
 		NavigateCommand = new Command<Type>(
