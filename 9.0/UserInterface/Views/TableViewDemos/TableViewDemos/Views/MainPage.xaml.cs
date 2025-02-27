8cac6d4821b6e744670d93fa49f748c48ab3774a
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-02-27 13:19:54 +0100

    Changes suggested by Copilot (transform a TableView to CollectionView)

--- 9.0/UserInterface/Views/TableViewDemos/TableViewDemos/Views/MainPage.xaml.cs
+++ 9.0/UserInterface/Views/TableViewDemos/TableViewDemos/Views/MainPage.xaml.cs
@@ -1,21 +1,40 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
 
-namespace TableViewDemos;
-
-public partial class MainPage : ContentPage
+namespace TableViewDemos
 {
-	public ICommand NavigateCommand { get; private set; }
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<MenuItem> MenuItems { get; set; }
+        public Command<Type> NavigateCommand { get; set; }
 
-	public MainPage()
-	{
-		InitializeComponent();
+        public MainPage()
+        {
+            InitializeComponent();
+            MenuItems = new ObservableCollection<MenuItem>
+            {
+                new MenuItem { Text = "Menu page", PageType = typeof(MenuIntentPage) },
+                new MenuItem { Text = "Settings page", PageType = typeof(SettingsIntentPage) },
+                new MenuItem { Text = "Form page", PageType = typeof(FormIntentPage) },
+                new MenuItem { Text = "Data page", PageType = typeof(DataIntentPage) },
+                new MenuItem { Text = "ImageCell page", PageType = typeof(ImageCellPage) },
+                new MenuItem { Text = "Right to left TableView", PageType = typeof(RightToLeftTablePage) }
+            };
+            NavigateCommand = new Command<Type>(async (pageType) => await NavigateToPage(pageType));
+            BindingContext = this;
+        }
 
-		NavigateCommand = new Command<Type>(
-			async (Type pageType) =>
-			{
-				Page page = (Page)Activator.CreateInstance(pageType);
-				await Navigation.PushAsync(page);
-			});
-		BindingContext = this;
-	}
+        private async Task NavigateToPage(Type pageType)
+        {
+            var page = (Page)Activator.CreateInstance(pageType);
+            await Navigation.PushAsync(page);
+        }
+    }
+
+    public class MenuItem
+    {
+        public string Text { get; set; }
+        public Color TextColor { get; set; } = Colors.Black;
+        public Type PageType { get; set; }
+    }
 }
