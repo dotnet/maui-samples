4d679e97fe25a1e5396f8ce8412bf7e33d989322
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-07-01 11:07:58 +0200

    More changes

--- 10.0/Apps/DeveloperBalance/MauiProgram.cs
+++ 10.0/Apps/DeveloperBalance/MauiProgram.cs
@@ -22,6 +22,14 @@ public static class MauiProgram
                 Microsoft.Maui.Controls.Handlers.Items.CollectionViewHandler.Mapper.AppendToMapping("KeyboardAccessibleCollectionView", (handler, view) =>
                 {
                     handler.PlatformView.SingleSelectionFollowsFocus = false;
+                }); 
+
+				Microsoft.Maui.Handlers.ContentViewHandler.Mapper.AppendToMapping(nameof(Pages.Controls.CategoryChart), (handler, view) =>
+                {
+                    if (view is Pages.Controls.CategoryChart && handler.PlatformView is Microsoft.Maui.Platform.ContentPanel contentPanel)
+                    {
+                        contentPanel.IsTabStop = true;
+                    }
                 });
 #endif
             })
--- 10.0/Apps/DeveloperBalance/Pages/Controls/CategoryChart.xaml.cs
+++ 10.0/Apps/DeveloperBalance/Pages/Controls/CategoryChart.xaml.cs
@@ -6,18 +6,4 @@ public partial class CategoryChart
 	{
 		InitializeComponent();
 	}
-
-    protected override void OnHandlerChanged()
-    {
-        base.OnHandlerChanged();
-
-        var chart = Chart.Handler?.PlatformView;
-
-#if WINDOWS
-        if (chart is Microsoft.Maui.Platform.ContentPanel contentPanel)
-        {
-            contentPanel.IsTabStop = true;
-        }
-#endif
-    }
 }
