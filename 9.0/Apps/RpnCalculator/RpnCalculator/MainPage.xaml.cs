d4fad20262f638be6351ef9bd3906f05142345ce
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2025-07-06 15:57:05 +0200

    Adjust RpnCalculator suggestions after review

--- 9.0/Apps/RpnCalculator/RpnCalculator/MainPage.xaml.cs
+++ 9.0/Apps/RpnCalculator/RpnCalculator/MainPage.xaml.cs
@@ -5,7 +5,29 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+    }
 
-        SizeChanged += (sender, args) => portrait.IsVisible = !(landscape.IsVisible = Width > Height);
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        
+        // Subscribe to SizeChanged event when page appears
+        SizeChanged += OnSizeChanged;
+        
+        // Set initial layout based on current dimensions
+        OnSizeChanged(this, EventArgs.Empty);
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        
+        // Unsubscribe from SizeChanged event when page disappears
+        SizeChanged -= OnSizeChanged;
+    }
+
+    private void OnSizeChanged(object? sender, EventArgs e)
+    {
+        portrait.IsVisible = !(landscape.IsVisible = Width > Height);
     }
 }
