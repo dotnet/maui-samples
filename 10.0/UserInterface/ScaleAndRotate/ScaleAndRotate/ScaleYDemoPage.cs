27fa57c75c75a2366dce2aaf36c093af8d824d04
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2025-07-06 13:11:20 +0200

    Migrate `ScaleAndRotate` sample

---
+++ 10.0/UserInterface/ScaleAndRotate/ScaleAndRotate/ScaleYDemoPage.cs
@@ -0,0 +1,136 @@
+﻿namespace ScaleAndRotate;
+
+class ScaleYDemoPage : ContentPage
+{
+    public ScaleYDemoPage()
+    {
+        this.Title = "ScaleY";
+
+        // Label to be transformed.
+        Label label = new Label
+        {
+            Text = "SCALEY",
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center
+        };
+
+        // Label and Slider for ScaleY property.
+        Label scaleSliderValue = new Label
+        {
+            VerticalTextAlignment = TextAlignment.Center
+        };
+        Grid.SetRow(scaleSliderValue, 0);
+        Grid.SetColumn(scaleSliderValue, 0);
+
+        Slider scaleSlider = new Slider
+        {
+            Maximum = 10
+        };
+        Grid.SetRow(scaleSlider, 0);
+        Grid.SetColumn(scaleSlider, 1);
+
+        // Set Bindings.
+        scaleSliderValue.BindingContext = scaleSlider;
+        scaleSliderValue.SetBinding(Label.TextProperty,
+            new Binding("Value", BindingMode.OneWay,
+                null, null, "ScaleY = {0:F1}"));
+
+        scaleSlider.BindingContext = label;
+        scaleSlider.SetBinding(Slider.ValueProperty,
+            new Binding("ScaleY", BindingMode.TwoWay));
+        
+        // Label and Slider for AnchorX property.
+        Label anchorxStepperValue = new Label
+        {
+            VerticalTextAlignment = TextAlignment.Center
+        };
+        Grid.SetRow(anchorxStepperValue, 1);
+        Grid.SetColumn(anchorxStepperValue, 0);
+
+        Stepper anchorxStepper = new Stepper
+        {
+            Maximum = 2,
+            Minimum = -1,
+            Increment = 0.5
+        };
+        Grid.SetRow(anchorxStepper, 1);
+        Grid.SetColumn(anchorxStepper, 1);
+
+        // Set bindings.
+        anchorxStepperValue.BindingContext = anchorxStepper;
+        anchorxStepperValue.SetBinding(Label.TextProperty,
+            new Binding("Value", BindingMode.OneWay,
+                null, null, "AnchorX = {0:F1}"));
+
+        anchorxStepper.BindingContext = label;
+        anchorxStepper.SetBinding(Stepper.ValueProperty,
+            new Binding("AnchorX", BindingMode.TwoWay));
+
+        // Label and Slider for AnchorY property.
+        Label anchoryStepperValue = new Label
+        {
+            VerticalTextAlignment = TextAlignment.Center
+        };
+        Grid.SetRow(anchoryStepperValue, 2);
+        Grid.SetColumn(anchoryStepperValue, 0);
+
+        Stepper anchoryStepper = new Stepper
+        {
+            Maximum = 2,
+            Minimum = -1,
+            Increment = 0.5
+        };
+        Grid.SetRow(anchoryStepper, 2);
+        Grid.SetColumn(anchoryStepper, 1);
+
+        // Set bindings.
+        anchoryStepperValue.BindingContext = anchoryStepper;
+        anchoryStepperValue.SetBinding(Label.TextProperty,
+            new Binding("Value", BindingMode.OneWay,
+                null, null, "AnchorY = {0:F1}"));
+
+        anchoryStepper.BindingContext = label;
+        anchoryStepper.SetBinding(Stepper.ValueProperty,
+            new Binding("AnchorY", BindingMode.TwoWay));
+
+        // Create controls grid
+        Grid controlsGrid = new Grid 
+        {
+            Padding = 10,
+            RowDefinitions = {
+                new RowDefinition { Height = GridLength.Auto },
+                new RowDefinition { Height = GridLength.Auto },
+                new RowDefinition { Height = GridLength.Auto }
+            },
+            ColumnDefinitions = {
+                new ColumnDefinition { Width = new GridLength (0.5, GridUnitType.Star) },
+                new ColumnDefinition { Width = new GridLength (0.5, GridUnitType.Star) }
+            },
+            Children = {
+                scaleSliderValue,
+                scaleSlider,
+                anchorxStepperValue,
+                anchorxStepper,
+                anchoryStepperValue,
+                anchoryStepper
+            }
+        };
+
+        // Assemble the page with proper layout.
+        this.Content = new Grid
+        {
+            RowDefinitions = {
+                new RowDefinition { Height = GridLength.Star },
+                new RowDefinition { Height = GridLength.Auto }
+            },
+            Children = {
+                label,
+                controlsGrid
+            }
+        };
+
+        // Set grid positions
+        Grid.SetRow(label, 0);
+        Grid.SetRow(controlsGrid, 1);
+    }
+}
