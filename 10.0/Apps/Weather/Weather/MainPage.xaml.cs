81e8ead97ed7f90e1d6a86d9d8b9b72eca58390d
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2025-07-09 10:29:38 +0200

    Migrate `Weather` sample

---
+++ 10.0/Apps/Weather/Weather/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace Weather;
+
+public partial class MainPage : ContentPage
+{
+    RestService _restService;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        _restService = new RestService();
+    }
+
+    async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
+        {
+            string requestUri = GenerateRequestUri(Constants.OpenWeatherMapEndpoint);
+            WeatherData? weatherData = await _restService.GetWeatherData(requestUri);
+            BindingContext = weatherData;
+        }
+    }
+
+    string GenerateRequestUri(string endpoint)
+    {
+        string requestUri = endpoint;
+        requestUri += $"?q={_cityEntry.Text}";
+        requestUri += "&units=imperial"; // or units=metric
+        requestUri += $"&appid={Constants.OpenWeatherMapAPIKey}"; // changed to lowercase 'appid'
+        return requestUri;
+    }
+}
