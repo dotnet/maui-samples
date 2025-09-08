80c033e6e2a9b4361816fd1bc8698dbbfd5e339d
David Ortinau <david.ortinau@microsoft.com>
2025-09-08 18:43:26 -0500

    live activity sample for iOS

---
+++ 10.0/PlatformIntegration/LiveActivityDemo/LiveActivityDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LiveActivityDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ 10.0/PlatformIntegration/LiveActivityDemo/native/LiveActivityAppHost/LiveActivityAppHost/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveActivityAppHost
+//
+//  Created by David Ortinau on 9/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
