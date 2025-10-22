0c9e8da6c264bd9780b4b6d1bc96e0fdc572bf9d
Mahmoud Elgohary <14031701+melgoharyme@users.noreply.github.com>
2025-10-22 18:00:50 +0300

    Simplify MainApplication constructor for improved clarity

--- Upgrading/CustomRenderer/MultiProject/Entry/Android/MainApplication.cs
+++ Upgrading/CustomRenderer/MultiProject/Entry/Android/MainApplication.cs
@@ -7,13 +7,8 @@ using Microsoft.Maui.Hosting;
 namespace CustomRenderer.Android
 {
 	[Application]
-	public class MainApplication : MauiApplication
-	{
-		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-			: base(handle, ownership)
-		{
-		}
-
+    public class MainApplication(IntPtr handle, JniHandleOwnership ownership) : MauiApplication(handle, ownership)
+    {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
 }
