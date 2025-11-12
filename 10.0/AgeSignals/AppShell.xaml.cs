963ceb735e078d80e1c4df0c0151cd48e5b79bef
praveenkumarkarunanithi <praveenkumar.karunanithi@syncfusion.com>
2025-11-12 16:12:30 +0530

    removing the old folder structuring

--- 10.0/AgeSignals/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace AgeSignals;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- 10.0/AgeSignals/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-using Foundation;
-
-namespace AgeSignals;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
