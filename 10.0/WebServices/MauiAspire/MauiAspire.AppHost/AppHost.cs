78ffd3b9d16d5c76c1777810bfb5403ff331bf74
Gerald Versluis <gerald.versluis@microsoft.com>
2025-11-24 12:46:17 +0100

    MauiAspire filter on host platform

--- 10.0/WebServices/MauiAspire/MauiAspire.AppHost/AppHost.cs
+++ 10.0/WebServices/MauiAspire/MauiAspire.AppHost/AppHost.cs
@@ -3,27 +3,31 @@ var builder = DistributedApplication.CreateBuilder(args);
 var webapi = builder.AddProject<Projects.SampleWebApi>("webapi");
 
 var mauiapp = builder.AddMauiProject("mauiapp", "../SampleMauiApp/SampleMauiApp.csproj");
-
-mauiapp.AddWindowsDevice()
-    .WithReference(webapi);
-
-mauiapp.AddMacCatalystDevice()
-    .WithReference(webapi);
-
 var blazorhybridapp = builder.AddMauiProject("blazorhybridapp", "../SampleBlazorHybridApp/SampleBlazorHybridApp.csproj");
-
-blazorhybridapp.AddWindowsDevice()
-    .WithReference(webapi);
-
-blazorhybridapp.AddMacCatalystDevice()
-    .WithReference(webapi);
-
 var hybridwebviewapp = builder.AddMauiProject("hybridwebviewapp", "../SampleHybridWebViewApp/SampleHybridWebViewApp.csproj");
 
-hybridwebviewapp.AddWindowsDevice()
-    .WithReference(webapi);
+if (OperatingSystem.IsWindows())
+{
+    mauiapp.AddWindowsDevice()
+        .WithReference(webapi);
 
-hybridwebviewapp.AddMacCatalystDevice()
-    .WithReference(webapi);
+    blazorhybridapp.AddWindowsDevice()
+        .WithReference(webapi);
+
+    hybridwebviewapp.AddWindowsDevice()
+        .WithReference(webapi);
+}
+
+if (OperatingSystem.IsMacOS())
+{
+    mauiapp.AddMacCatalystDevice()
+        .WithReference(webapi);
+
+    blazorhybridapp.AddMacCatalystDevice()
+        .WithReference(webapi);
+
+    hybridwebviewapp.AddMacCatalystDevice()
+        .WithReference(webapi);
+}
 
 builder.Build().Run();
