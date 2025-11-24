7074d8f245fa3e95b9d065d5ce068ee8a3e4c216
Dhivya-SF4094 <127717131+Dhivya-SF4094@users.noreply.github.com>
2025-11-24 17:12:56 +0530

    updated Default and container sample

--- 10.0/UserInterface/Layouts/SafeAreaDemos/SafeAreaDemos/Views/Example2RespectAll.xaml.cs
+++ 10.0/UserInterface/Layouts/SafeAreaDemos/SafeAreaDemos/Views/Example2RespectAll.xaml.cs
@@ -1,28 +1,10 @@
-using SafeAreaDemos.ViewModels;
-
 namespace SafeAreaDemos.Views;
 
 public partial class Example2RespectAll : ContentPage
 {
-	private Example2RespectAllViewModel viewModel;
-
 	public Example2RespectAll()
 	{
 		InitializeComponent();
-
-		viewModel = new Example2RespectAllViewModel();
-		BindingContext = viewModel;
-	}
-
-	void OnSearchTextChanged(object sender, TextChangedEventArgs e)
-	{
-		viewModel.FilterContacts(e.NewTextValue);
-	}
-
-	void OnContactListTapped(object sender, EventArgs e)
-	{
-		// Dismiss the keyboard when tapping on the contact list
-		contactSearchBar.Unfocus();
 	}
 
 	async void Button_Clicked(object sender, EventArgs e)
