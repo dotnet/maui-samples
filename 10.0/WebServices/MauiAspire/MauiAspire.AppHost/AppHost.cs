f471659eead77a2edf5a1c0968e9b2a6285bb27c
Gerald Versluis <gerald.versluis@microsoft.com>
2025-11-24 13:05:19 +0100

    MauiAspire add DevTunnels and Android

--- 10.0/WebServices/MauiAspire/MauiAspire.AppHost/AppHost.cs
+++ 10.0/WebServices/MauiAspire/MauiAspire.AppHost/AppHost.cs
@@ -2,6 +2,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var webapi = builder.AddProject<Projects.SampleWebApi>("webapi");
 
+var publicDevTunnel = builder.AddDevTunnel("devtunnel-public")
+    .WithAnonymousAccess() // All ports on this tunnel default to allowing anonymous access
+    .WithReference(webapi.GetEndpoint("https"));
+
 var mauiapp = builder.AddMauiProject("mauiapp", "../SampleMauiApp/SampleMauiApp.csproj");
 var blazorhybridapp = builder.AddMauiProject("blazorhybridapp", "../SampleBlazorHybridApp/SampleBlazorHybridApp.csproj");
 var hybridwebviewapp = builder.AddMauiProject("hybridwebviewapp", "../SampleHybridWebViewApp/SampleHybridWebViewApp.csproj");
@@ -23,11 +27,27 @@ if (OperatingSystem.IsMacOS())
     mauiapp.AddMacCatalystDevice()
         .WithReference(webapi);
 
+    mauiapp.AddiOSSimulator()
+        .WithOtlpDevTunnel()
+        .WithReference(webapi, publicDevTunnel);
+
     blazorhybridapp.AddMacCatalystDevice()
         .WithReference(webapi);
 
+    blazorhybridapp.AddiOSSimulator()
+        .WithOtlpDevTunnel()
+        .WithReference(webapi, publicDevTunnel);
+
     hybridwebviewapp.AddMacCatalystDevice()
         .WithReference(webapi);
+
+    hybridwebviewapp.AddiOSSimulator()
+        .WithOtlpDevTunnel()
+        .WithReference(webapi, publicDevTunnel);
 }
+
+mauiapp.AddAndroidEmulator()
+    .WithOtlpDevTunnel()
+    .WithReference(webapi, publicDevTunnel);
 
 builder.Build().Run();
